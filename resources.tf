fcc0341c6d442fda0373f4dd743d31c3cb32faad
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-02 02:12:46 +0500

    Configure the aws_instance to match my region

--- resources.tf
+++ resources.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "example" {
-  ami           = "ami-00f07845aed8c0ee7" # Replace with a valid AMI ID for your region
+resource "aws_instance" "main_server" {
+  ami           = "ami-000defd1c33d4d17b"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "example-instance"
+    Name = "MainInstance"
   }
 }
