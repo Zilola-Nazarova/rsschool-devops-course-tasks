b80c59748794ab4c56fba6afeaa6441f7e89f3f6
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-07 00:01:47 +0500

    Create 1 route table for Public Subnets and 2 route tables for Private Subnets

---
+++ route-table.tf
@@ -0,0 +1,29 @@
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.task_2_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public_internet_gateway.id
+  }
+
+  tags = {
+    Name    = "Public Route Table"
+    Project = "Task 2"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  count      = length(var.cidr_private_subnet)
+  vpc_id     = aws_vpc.task_2_vpc.id
+  depends_on = [aws_nat_gateway.nat_gateway]
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
+  }
+
+  tags = {
+    Name    = "Private Route Table ${count.index + 1}"
+    Project = "Task 2"
+  }
+}
