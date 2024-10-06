a2193d019b99d8e635ee107b5edc5f46564b0c26
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-06 23:59:12 +0500

    Update the tags and resource names to follow one structure so that the code is more readable

--- internet-gateway.tf
+++ internet-gateway.tf
@@ -2,6 +2,6 @@ resource "aws_internet_gateway" "public_internet_gateway" {
   vpc_id = aws_vpc.task_2_vpc.id
 
   tags = {
-    Name = "Task 2 Internet Gateway"
+    Name = "Internet Gateway for Public Subnets"
   }
 }
--- setup-subnet.tf
+++ setup-subnet.tf
@@ -1,21 +1,21 @@
-resource "aws_subnet" "task_2_public_subnets" {
+resource "aws_subnet" "public_subnets" {
   count             = length(var.cidr_public_subnet)
   vpc_id            = aws_vpc.task_2_vpc.id
   cidr_block        = element(var.cidr_public_subnet, count.index)
   availability_zone = element(var.eu_availability_zone, count.index)
 
   tags = {
-    Name = "Task 2 Subnet Public #${count.index + 1}"
+    Name = "Public Subnet #${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "task_2_private_subnets" {
+resource "aws_subnet" "private_subnets" {
   count             = length(var.cidr_private_subnet)
   vpc_id            = aws_vpc.task_2_vpc.id
   cidr_block        = element(var.cidr_private_subnet, count.index)
   availability_zone = element(var.eu_availability_zone, count.index)
 
   tags = {
-    Name = "Task 2 Subnet Private #${count.index + 1}"
+    Name = "Private Subnet #${count.index + 1}"
   }
 }
