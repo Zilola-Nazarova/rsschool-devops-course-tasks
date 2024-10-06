0a3d9aee805567e0526e5647d01fb4a36f69a46f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-06 22:17:04 +0500

    Create 2 private and 2 public subnets and all necessary variables

---
+++ setup-subnet.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "task_2_public_subnets" {
+  count             = length(var.cidr_public_subnet)
+  vpc_id            = aws_vpc.task_2_vpc.id
+  cidr_block        = element(var.cidr_public_subnet, count.index)
+  availability_zone = element(var.eu_availability_zone, count.index)
+
+  tags = {
+    Name = "Subnet Public #${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "task_2_private_subnets" {
+  count             = length(var.cidr_private_subnet)
+  vpc_id            = aws_vpc.task_2_vpc.id
+  cidr_block        = element(var.cidr_private_subnet, count.index)
+  availability_zone = element(var.eu_availability_zone, count.index)
+
+  tags = {
+    Name = "Subnet Private #${count.index + 1}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -45,3 +45,21 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "cidr_public_subnet" {
+  description = "Public Subnet CIDR values"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "cidr_private_subnet" {
+  description = "Private Subnet CIDR values"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "eu_availability_zone" {
+  description = "Private Subnet CIDR values"
+  type        = list(string)
+  default     = ["eu-north-1a", "eu-north-1b"]
+}
