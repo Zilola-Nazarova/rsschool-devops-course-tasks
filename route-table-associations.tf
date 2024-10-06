10375928063b306f6aa4ebc2129f7836867c4954
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-07 00:55:06 +0500

    Set up associations between the route tables and corresponding subnets

---
+++ route-table-associations.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(var.cidr_public_subnet)
+  depends_on     = [aws_subnet.public_subnets, aws_route_table.public_route_table]
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = length(var.cidr_private_subnet)
+  depends_on     = [aws_subnet.private_subnets, aws_route_table.private_route_table]
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.private_route_table[count.index].id
+}
