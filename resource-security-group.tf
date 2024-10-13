f5c0132dbbf0989973c95ac4ec464bec14791680
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-14 02:02:57 +0500

    Create 2 different SGs for private/public subnets with correstponding rules

--- resource-security-group.tf
+++ resource-security-group.tf
@@ -1,61 +1,89 @@
-resource "aws_security_group" "security_group" {
-  name        = "SG for Task 2 VPC"
-  description = "To allow SSH, HTTP, HTTPS inbound traffic"
-
-  egress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
-
-  ingress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = "HTTPS from VPC"
-      from_port        = 443
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 443
-    },
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = "HTTP from VPC"
-      from_port        = 80
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 80
-    },
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = "SSH from VPC"
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    }
-  ]
-
-  vpc_id     = aws_vpc.task_2_vpc.id
-  depends_on = [aws_vpc.task_2_vpc]
+// CREATE PUBLIC SECURITY GROUP FOR PUBLIC SUBNETS
+resource "aws_security_group" "public_sg" {
+  name        = "public-sg"
+  description = "Public internet access"
+  vpc_id      = aws_vpc.task_2_vpc.id
+  depends_on  = [aws_vpc.task_2_vpc]
 
   tags = {
-    Name = "Security Group for Task 2 VPC"
+    Name    = "Public Security Group for VPC"
+    Project = "Task 2"
   }
+}
+
+// ALLOW ALL OUTBOUND TRAFFIC UNRESTRICTED
+resource "aws_security_group_rule" "public_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.public_sg.id
+}
+
+// ALLOW INBOUND TRAFFIC: SSH, HTTP, HTTPS
+resource "aws_security_group_rule" "public_in_ssh" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.public_sg.id
+}
+
+resource "aws_security_group_rule" "public_in_http" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.public_sg.id
+}
+
+resource "aws_security_group_rule" "public_in_https" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.public_sg.id
+}
+
+// CREATE PRIVATE SECURITY GROUP FOR PRIVATE SUBNETS
+resource "aws_security_group" "private_sg" {
+  name        = "private-sg"
+  description = "Private internet access"
+  vpc_id      = aws_vpc.task_2_vpc.id
+  depends_on  = [aws_vpc.task_2_vpc]
+
+  tags = {
+    Name    = "Private Security Group for VPC"
+    Project = "Task 2"
+  }
+}
+
+// ALLOW ALL OUTBOUND TRAFFIC UNRESTRICTED
+resource "aws_security_group_rule" "private_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.private_sg.id
+}
+
+// RESTRICT INBOUND TRAFFIC TO VPC'S CIDR BLOCKS
+resource "aws_security_group_rule" "private_in_ssh" {
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "-1"
+  cidr_blocks = [aws_vpc.task_2_vpc.cidr_block]
+
+  security_group_id = aws_security_group.private_sg.id
 }
